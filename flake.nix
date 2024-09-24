fda97c2d1cf902ba174777330ae1f3f3c64835be
zestsystem <mikeyim@unitetheculture.org>
2024-09-23 22:21:36 -0700

    feat: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Get Typesense working";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      corepackEnable = pkgs.runCommand "corepack-enable" {} ''
+        mkdir -p $out/bin
+        ${pkgs.nodejs-18_x}/bin/corepack enable --install-directory $out/bin
+      '';
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            corepackEnable
+            docker-compose
+          ];
+          shellHook = ''
+
+          '';
+        };
+      };
+    });
+}
